925e72f13d6f63237fc127d2baa86915bd524829
Amarnath <amarnath@99gift.in>
2025-11-12 15:27:47 +0530

    edfd

--- src/models/contactUs.model.js
+++ src/models/contactUs.model.js
@@ -5,7 +5,7 @@ const contactUs = new mongoose.Schema({
   firstname: { type: String, required: true, trim: true },
   lastname: { type: String, required: true, trim: true },
   phoneNo: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
+  email: { type: String, required: true, lowercase: true },
   subject: { type: String, required: true,},
   message: { type: String, required: true,},
  
